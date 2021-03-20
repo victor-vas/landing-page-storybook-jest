3d7b403860d9bfef3535a7de7f57139b6ee896ef
victor-vas <victorvasconcelos88@gmail.com>
2021-03-20 19:56:09 -0300

    storybook component Heading

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,11 +1,24 @@
 import P from 'prop-types';
 import * as S from './styles';
 
-export const Heading = ({ children, light = false }) => {
-  return <S.Title light={light}>{children}</S.Title>;
+export const Heading = ({
+  children,
+  colorDark = true,
+  as = 'h1',
+  size = 'huge',
+  uppercase = false,
+}) => {
+  return (
+    <S.Title colorDark={colorDark} as={as} size={size} uppercase={uppercase}>
+      {children}
+    </S.Title>
+  );
 };
 
 Heading.propTypes = {
   children: P.node.isRequired,
-  light: P.bool.isRequired,
+  colorDark: P.bool,
+  as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  size: P.oneOf(['small', 'medium', 'big', 'huge']),
+  uppercase: P.bool,
 };
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,15 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
   }
 
   html {
-    font-size: 62.5%large;
+    font-size: 62.5%;
   }
 
   body {
